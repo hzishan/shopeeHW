cfc8978d5a0fe92f9231b2ac7fcd54274733084d
hzishan <ncnu10813@gmail.com>
2023-07-12 00:57:31 +0800

    add layout into page

---
+++ src/components/layout/PaymentLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DefaultLayout from "../components/layout/DefaultLayout";
+
+const PaymentLayout =()=>{
+    return <div>PaymentLayout</div>
+};
+
+export default PaymentLayout;
