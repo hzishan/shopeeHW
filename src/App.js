a44a25a5665ebb259b4683cfdf9fd54df00302c8
hzishan <ncnu10813@gmail.com>
2023-07-12 00:59:30 +0800

    route test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom"
+import HomePage from "./pages/Homepage"
+import CartPage from "./pages/CartPage"
+import CheckoutPage from "./pages/CheckoutPage"
+import ProductCollectionPage from "./pages/ProductCollectionPage"
+import ProductPage from "./pages/ProductPage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+const App = ()=>{
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/mall"/>} />
+        <Route exact path="/mall" element={<HomePage/>} />
+        <Route exact path="/cart" element={<CartPage/>} />
+        <Route exact path="/checkout" element={<CheckoutPage/>} />
+        <Route exact path="/mall/:categoryName" element={<ProductCollectionPage/>} />
+        <Route exact path="/:productName" element={<ProductPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
